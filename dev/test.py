3bf6b36e992d67d575a09e4cacef6b624bd5ad5f
ajaxj <ajaxj@qq.com>
2014-11-12 12:05:36 +0800

    change test

--- dev/test.py
+++ dev/test.py
@@ -1,7 +1,14 @@
 # -*-coding:utf-8 -*-
 import time
+import thread
+
+def timer(no,interval):
+	print "timer"
 
 
+def test():
+	timer(1,2)
+	timer(1,1)
 
 if __name__ == "__main__":
-	print "test"
+	test()
